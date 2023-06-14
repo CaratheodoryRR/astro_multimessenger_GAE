106299dd51bf2ffd6d8a2f135039c7c6b93e592c
CaratheodoryRR <cabanillas.christian@pucp.edu.pe>
2023-06-13 22:32:44 -0500

    Command line arguments included
    The script now uses the argparse module to handle CLI arguments. All of these arguments have default values so you can run it without specifying them. Run python <script.py> -h for more information

--- scripts/complete_propagation_Dolag_JF12.py
+++ scripts/complete_propagation_Dolag_JF12.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from crpropa import *
 import numpy as np
-import UHECRs_sim_f as cpf
+from src import UHECRs_sim_f as cpf
+from src import auger_data_he as pao
+import argparse
+
 
 # Print iterations progress
 # Copied from https://stackoverflow.com/questions/3173320/text-progress-bar-in-terminal-with-block-characters
@@ -44,16 +47,48 @@ def sim_settings(sim, model = IRB_Gilmore12(), minE = 1.*EeV):
     sim.add(MinimumEnergy(minE))
 
 
-dirOutput = './'
+parser = argparse.ArgumentParser(
+                    description='Cosmic ray propagation on both extragalactic (Dolag) and galactic (JF12) magnetic fields',
+                    epilog='GAE-PUCP Astroparticle Physics')
+
+parser.add_argument('-s', '--srcPath', default='../data/EG_sources.txt',
+                    help='Path to the EG sources text file (default: %(default)s)')
+parser.add_argument('-D', '--dolagPath', default='../data/dolag_B_54-186Mpc_440b.raw',
+                    help='Path to the Dolag EGMF raw file (default: %(default)s)')
+parser.add_argument('-o', '--outDir', default='./',
+                    help='Directory to store the CRs output information (default: %(default)s)')
+parser.add_argument('-g', '--minEnergy', default=18, type=float, 
+                    help='Minimum emission energy exponent [10^g eV] (default: %(default)s)')
+parser.add_argument('-e', '--maxEnergy', default=21, type=float, 
+                    help='Maximum emission energy exponent [10^e eV] (default: %(default)s)')
+parser.add_argument('-x', '--stopEnergy', type=float,
+                    help='CRs below this energy exponent are discarded [10^d eV] (default: minEnergy)')
+parser.add_argument('-n', '--num', default=10**5, type=int,
+                    help='Total number of emitted cosmic rays (default: %(default)s)')
+parser.add_argument('-b', '--bFactor', default=1e-4, type=float,
+                    help='Scale factor for the EGMF (default: %(default)s)')
+
+# TODO: Include an optional tau argument for vMF Emission Distribution
+
+args = parser.parse_args()
+
+dirOutput = args.outDir
 cpf.check_dir(dirOutput)
 fnameOutput = 'events'
 
 
-sources = np.genfromtxt('EG_sources.txt',names=True)
+sources = np.genfromtxt(args.srcPath, names=True)
 
 # source
 sourcelist = SourceList()
 tau = 100. # Concentration parameter
+
+if not args.stopEnergy:
+    args.stopEnergy = args.minEnergy
+    
+minE = 10.**args.minEnergy * eV
+maxE = 10.**args.maxEnergy * eV
+stopE = 10.**args.stopEnergy * eV
 
 ##############################################################################################################
 #                                   EXTRAGALACTIC PROPAGATION (DOLAG MODEL)
@@ -62,13 +97,13 @@ r_galaxy = 20.*kpc
 
 
 # magnetic field setup
-filename_bfield = 'dolag_B_54-186Mpc_440b.raw'
+filename_bfield = args.dolagPath
 gridSize = 440
 size = 186*Mpc
-b_factor = 1e-3
+b_factor = args.bFactor
 spacing = size/(gridSize)
 obsPosition = Vector3d(0,0,0)                       
-boxOrigin = Vector3d(-0.5*size,-0.5*size,-0.5*size) 
+boxOrigin = Vector3d(-0.5*size, -0.5*size, -0.5*size) 
 
 vgrid = Grid3f(boxOrigin, gridSize, spacing )
 loadGrid(vgrid, filename_bfield, b_factor)
@@ -79,8 +114,7 @@ Dolag_field = MagneticFieldGrid(vgrid)
 sim = ModuleList()
 sim.add(PropagationBP(Dolag_field, 1e-4, 1.*kpc, 1.*Mpc))
 
-sim_settings(sim=sim)
-
+sim_settings(sim=sim, minE=stopE)
 
 # observer and output
 EG_obs = Observer()
@@ -102,12 +136,12 @@ for source in sources:
     #s.add(SourceDirectedEmission( v.getUnitVector() * (-1.), tau )) # vMF distribution
     s.add(SourceDirection( v.getUnitVector() * (-1.))) # Emission in one direction
     s.add(SourceParticleType(nucleusId(1, 1)))
-    s.add(SourcePowerLawSpectrum(1 * EeV, 1000 * EeV, -2.3))
+    s.add(SourcePowerLawSpectrum(minE, maxE, -2.3))
     sourcelist.add(s, 1)
 
 # run simulation
 sim.setShowProgress(True)
-sim.run(sourcelist, 10**5)
+sim.run(sourcelist, args.num)
 
 output.dump('{}{}_Dolag.txt.gz'.format(dirOutput,fnameOutput))
 #output.close()
@@ -128,12 +162,12 @@ JF12_field.randomTurbulent()
 sim = ModuleList()
 sim.add(PropagationBP(JF12_field, 1e-4, 0.1*pc, 1.*kpc))
 
-sim_settings(sim=sim)
+sim_settings(sim=sim, minE=stopE)
 
 
 # observer and output
 G_obs = Observer()
-G_obs.add(ObserverSurface( Sphere(Vector3d(0), r_obs) ))
+G_obs.add(ObserverSurface( Sphere(Vector3d(-8.5*kpc, 0, 0), r_obs) ))
 output = TextOutput('{}{}_JF12.txt'.format(dirOutput,fnameOutput), Output.Event3D)
 G_obs.onDetection( output )
 sim.add(G_obs)
