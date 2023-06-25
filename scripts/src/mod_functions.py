0ff5d508bae83261af91429df461dc7e6bb254a9
CaratheodoryRR <cabanillas.christian@pucp.edu.pe>
2023-06-25 17:07:32 -0500

    New celestial coordinates handler
    You can now specify the coordinates in the command line using the -C flag. The supported coordinates are: galactic, supergalactic, icrs, cartesian, spherical. All of them are supposed to be under the ICRS reference frame. By convention, angular and spatial positions must be in radians and Mpc, respectively. The headers on the file containing the source positions must be: \n-) cartesian: X, Y, and Z \n-) spherical: R, Theta, Phi\n-) galactic: Longitude, Latitude, Distance\n-) supergalactic: SLatitude, SLongitude, Distance\n-)ICRS: RA, DEC, Distance

--- scripts/src/mod_functions.py
+++ scripts/src/mod_functions.py
@@ -1,5 +1,7 @@
 import numpy as np
 import auger_data_he as pao
+from astropy.coordinates import SkyCoord
+import astropy.units as u
 
 def event_counter_by_energy(filename, bins = pao.ebins_, col = 2):
     
@@ -8,3 +10,45 @@ def event_counter_by_energy(filename, bins = pao.ebins_, col = 2):
     counts = np.histogram(data, bins = bins)[0]
     
     return counts
+
+
+def coordinate_transformation_handler(sourcePositions, coords):
+    if coords == 'galactic':
+        sourcePositions = from_galactic_to_cartesian(sourcePositions)
+    elif coords == 'icrs':
+        sourcePositions = from_icrs_to_cartesian(sourcePositions)
+    elif coords == 'supergalactic':
+        sourcePositions = from_supergalactic_to_cartesian(sourcePositions)
+    return sourcePositions
+
+def to_named_cartesian(cartCoords):
+    cartCoords = [tuple(x) for x in cartCoords]
+    
+    return np.array(cartCoords, dtype=[('X', np.float64), ('Y', np.float64), ('Z', np.float64)])
+
+def from_galactic_to_cartesian(sourcePositions):
+    galCoords = SkyCoord(sourcePositions['Longitude']*u.radian, 
+                         sourcePositions['Latitude']*u.radian,
+                         sourcePositions['Distance']*u.Mpc,
+                         frame='galactic')
+    cartCoords = to_named_cartesian(galCoords.icrs.cartesian.xyz.value.T)
+    
+    return cartCoords
+    
+def from_supergalactic_to_cartesian(sourcePositions):
+    sgalCoords = SkyCoord(sourcePositions['SLongitude']*u.radian, 
+                          sourcePositions['SLatitude']*u.radian,
+                          sourcePositions['Distance']*u.Mpc,
+                          frame='supergalactic')
+    cartCoords = to_named_cartesian(sgalCoords.icrs.cartesian.xyz.value.T)
+    
+    return cartCoords
+
+def from_icrs_to_cartesian(sourcePositions):
+    icrsCoords = SkyCoord(sourcePositions['RA']*u.radian, 
+                          sourcePositions['DEC']*u.radian,
+                          sourcePositions['Distance']*u.Mpc,
+                          frame='icrs')
+    cartCoords = to_named_cartesian(icrsCoords.cartesian.xyz.value.T)
+    
+    return cartCoords
