39e6db33e82a6d5f538b39188e75cfbdab664084
CaratheodoryRR <cabanillas.christian@pucp.edu.pe>
2024-01-18 23:08:18 -0500

    Ugly print partially fixed

--- scripts/src/optimization/objective_functions.py
+++ scripts/src/optimization/objective_functions.py
@@ -15,6 +15,7 @@ orderedNuclei = sorted(A_Z.keys(), key=lambda z: A_Z[z])
 
 def chi2_obj_func(runPropFunc, sample, **kwargs):
     chi2Container = []
+    parts = kwargs.get('parts') if (kwargs.get('parts') is not None) else 1
     for fracs, (rcut, alpha) in tqdm(zip(sample[:,:-2], sample[:,-2:])):
         nucleiDict = dict(zip(orderedNuclei, fracs))
         with hidden_prints():
@@ -22,7 +23,9 @@ def chi2_obj_func(runPropFunc, sample, **kwargs):
                         rcut=rescale_from_normal(interval=rcutRange, value=rcut),
                         alpha=rescale_from_normal(interval=alphaRange, value=alpha),
                         **kwargs)
-        print '\033[A\r',
+        
+        
+        print parts*'\033[A'+'\r',
         eventFiles = Path(kwargs.get('outDir')).glob('**/*prop*.dat')
         simN = events_from_files(fileNames=eventFiles)
         chi2 = err_parameter_handler(errorType='chi2', simN=simN)
