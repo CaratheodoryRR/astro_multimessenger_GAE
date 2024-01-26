9b4696c1691aa2ddd3ba03afaa9830293d40b839
CaratheodoryRR <cabanillas.christian@pucp.edu.pe>
2024-01-26 17:52:27 -0500

    Chi2 restricted to custom bins

--- scripts/src/optimization/objective_functions.py
+++ scripts/src/optimization/objective_functions.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 from pathlib import Path
 
 from ..UHECRs_sim_f import A_Z
+from  .. import auger_data_he as pao
 from ..utils.error_functions import err_parameter_handler
 from ..utils.general import (events_from_files, 
                              rescale_from_normal,
@@ -13,7 +14,7 @@ rcutRange = [19., 21.]
 alphaRange = [1., 3.]
 orderedNuclei = sorted(A_Z.keys(), key=lambda z: A_Z[z])
 
-def chi2_obj_func(runPropFunc, sample, pattern, **kwargs):
+def chi2_obj_func(runPropFunc, sample, pattern, bins=pao.ebins, **kwargs):
     chi2Container = []
     parts = kwargs.get('parts') if (kwargs.get('parts') is not None) else 1
     for fracs, (rcut, alpha) in tqdm(zip(sample[:,:-2], sample[:,-2:])):
@@ -27,8 +28,9 @@ def chi2_obj_func(runPropFunc, sample, pattern, **kwargs):
         
         print parts*'\033[A'+'\r',
         eventFiles = Path(kwargs.get('outDir')).glob(pattern)
-        simN, lnAStats = events_from_files(fileNames=eventFiles)
-        chi2 = err_parameter_handler(errorType='chi2', simN=simN)
+        simN, lnAStats = events_from_files(fileNames=eventFiles, bins=bins)
+        idx = np.abs(pao.ebins-bins[0]).argmin()
+        chi2 = err_parameter_handler(errorType='chi2', simN=simN, paoN=pao.auger[idx:idx+len(bins)])
         chi2Container.append(chi2)
     print parts*'\n'
     return np.array(chi2Container)
