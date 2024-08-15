10e5fcf5ed1dbaf2a629e95c688883e427b522ff
CaratheodoryRR <cabanillas.christian@pucp.edu.pe>
2024-08-15 12:07:29 -0500

    Minor modification
    - Removing unused variables
    - Removing garbage collector module `gc`
    - Adding f-strings
    - Changing tqdm argument to show bar progress

--- scripts/src/optimization/objective_functions.py
+++ scripts/src/optimization/objective_functions.py
@@ -1,4 +1,3 @@
-import gc
 import numpy as np
 
 from tqdm import tqdm
@@ -17,23 +16,23 @@ orderedNuclei = sorted(A_Z.keys(), key=lambda z: A_Z[z])
 
 def chi2_obj_func(runPropFunc, sample, pattern, bins=pao.ebins, **kwargs):
     chi2Container = []
-    parts = kwargs.get('parts') if (kwargs.get('parts') is not None) else 1
-    for fracs, (rcut, alpha) in tqdm(zip(sample[:,:-2], sample[:,-2:])):
+
+    for fracs, (rcut, alpha) in zip(tqdm(sample[:,:-2]), sample[:,-2:]):
         nucleiDict = dict(zip(orderedNuclei, fracs))
-        print( 'Fracs: {}'.format(dict(zip(orderedNuclei, ['%.2e' % p for p in fracs/fracs.sum()]))) )
+        
+        print(f"Fracs: {dict(zip(orderedNuclei, [f'{p:.1%}' for p in fracs/fracs.sum()]))}")
         with hidden_prints():
             runPropFunc(yamlFile=nucleiDict,
                         rcut=rescale_from_normal(interval=rcutRange, value=rcut),
                         alpha=rescale_from_normal(interval=alphaRange, value=alpha),
                         **kwargs)
         
-        
         eventFiles = Path(kwargs.get('outDir')).glob(pattern)
-        simN, lnAStats, lnAErrors = events_from_files(fileNames=eventFiles, bins=bins)
+        simN, _, __ = events_from_files(fileNames=eventFiles, bins=bins)
         idx = np.abs(pao.ebins-bins[0]).argmin()
-        print('Events per bin: {}'.format(simN))
+        print(f'Events per bin: {simN}')
         chi2 = err_parameter_handler(errorType='chi2', simN=simN, paoN=pao.auger[idx:idx+len(bins)])
         chi2Container.append(chi2)
-        print('Associated chi2: {:.2f}'.format(chi2))
-        gc.collect()
+        print(f'Associated chi2: {chi2:.2f}')
+
     return np.array(chi2Container)
