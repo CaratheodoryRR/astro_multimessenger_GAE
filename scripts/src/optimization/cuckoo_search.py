d8b5b25d593fddf04cf73996f603eee42bbcd19c
CaratheodoryRR <cabanillas.christian@pucp.edu.pe>
2025-10-27 02:11:17 +0000

    Saving state checkpoints

--- scripts/src/optimization/cuckoo_search.py
+++ scripts/src/optimization/cuckoo_search.py
@@ -1,69 +1,134 @@
-import numpy as np
-
+import re
+import pickle
 from pathlib import Path
+
+import numpy as np
 from scipy.stats import (levy_stable, uniform, norm)
 
 from ..utils.general import rescale_from_normal
 from .objective_functions import (rcutRange, alphaRange)
 
-def save_checkpoint(checkpointArr, outDir, gen):
-    numOfParams = checkpointArr.shape[1]-1
-    header = (22*' ').join(['param{}'.format(i+1) for i in range(numOfParams)] + ['chi2'])
-    np.savetxt(fname=outDir.joinpath('generation_{}.checkpoint'.format(gen)),
-               X=checkpointArr,
-               header=header,
-               delimiter='    ')
+regChkPattern = re.compile(r'\d+')
+extract_checkpoint_generation = lambda checkpointFileName: int(regChkPattern.search(checkpointFileName).group())
 
-def load_checkpoint(checkpointFile):
-    assert Path(checkpointFile).exists, 'Checkpoint file does not exist!'
-    arr = np.loadtxt(fname=checkpointFile)
-    return arr
+class checkpoint_state:
+    checkpointPickled = 'generation_state.pkl'
+
+    def __init__(self, checkpointDir, igen=0, data=None):
+        self.igen = igen
+        self._data = data
+        self.__checkpointDir = Path(checkpointDir)
+        self.pickleFile = self.__checkpointDir.joinpath(checkpoint_state.checkpointPickled)
+
+    def update_gen_counter(self):
+        self.igen += 1
+
+    @property
+    def data(self):
+        if self._data is not None:
+            return self._data.copy()
+
+    @data.setter
+    def data(self, value):
+        if (not isinstance(value, np.ndarray)) and (value.ndim != 2):
+            raise ValueError("Data must be a 2D numpy array!")
+        self._data = value.copy()
+
+    def save_state(self, fileType):
+        if fileType.lower() == 'text':
+            self._save_state_to_txt()
+        elif fileType.lower() == 'pickle':
+            self._pickle_dump_state()
+        else:
+            raise ValueError("Only 'text' and 'pickle' file types supported!")
+
+    def load_state(self, checkpointFile):
+        suffix = Path(checkpointFile).suffix.lower()
+        if suffix == '.checkpoint':
+            self._load_state_from_txt(textFile=checkpointFile)
+        elif suffix in {'.pkl', '.pickle'}:
+            self.pickleFile = Path(checkpointFile)
+            self._pickle_load_state()
+        else:
+            raise ValueError("File must be in an appropriate format extension (.checkpoint, .pkl, or .pickle)")
+
+    def _save_state_to_txt(self):
+        numOfParams = self.data.shape[1]-1
+        header = (22*' ').join([f'param{i+1}' for i in range(numOfParams)] + ['chi2'])
+        np.savetxt(fname=self.__checkpointDir.joinpath(f'generation_{self.igen}.checkpoint'),
+                    X=self.data,
+                    header=header,
+                    delimiter='    ')
+
+    def _load_state_from_txt(self, textFile):
+        if not Path(textFile).exists():
+            raise FileNotFoundError("Checkpoint text file does not exist!")
+        self.data = np.loadtxt(fname=textFile)
+        self.igen = extract_checkpoint_generation(Path(textFile).name)
+
+    def _pickle_dump_state(self):
+        with open(self.pickleFile, 'wb') as handle:
+            pickle.dump(self, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    def _pickle_load_state(self):
+        if not self.pickleFile.exists():
+            raise FileNotFoundError("Checkpoint pickle file does not exist!")
+        with open(self.pickleFile, 'rb') as handle:
+            loadedState = pickle.load(handle)
+            self.__dict__.update(loadedState.__dict__)
+
+def find_ordered_checkpoints(checkpointDir):
+    checkpointFiles = Path(checkpointDir).glob('*.checkpoint')
+
+    orderedCheckpointFiles = sorted(checkpointFiles, key=extract_checkpoint_generation)
+
+    return orderedCheckpointFiles
 
 def from_raw_to_real(arr):
     arrCopy = arr.copy()
     arrCopy[:,:-3] = arr[:,:-3]/np.sum(arr[:,:-3], axis=1).reshape(-1,1)
     arrCopy[:,-3] = rescale_from_normal(interval=rcutRange, value=arr[:,-3])
     arrCopy[:,-2] = rescale_from_normal(interval=alphaRange, value=arr[:,-2])
-    
+
     return arrCopy
 
 def periodic_bc(r, *args):
-    
     wrapped = []
     for I, xi in zip(args, r.T):
-        
         xmin, xmax = I
         dx = xmax - xmin
-    
+
         xf = np.where( (xi>xmax) | (xi<xmin), xmin+(xi%dx), xi)
         wrapped.append(xf)
-    
+
     return np.array(wrapped).T
 
 def levy_advance(r0, scale, **kwargs):
-    
     uVec = norm.rvs(size=r0.size).reshape(r0.shape)
     uVec /= np.linalg.norm(uVec, axis=1).reshape(-1,1)
-    
+
     stepSize = scale*levy_stable.rvs(size=r0.shape[0], **kwargs).reshape(-1,1)
-        
+
     return r0 + stepSize*uVec
 
 def gen_population(size, *args):
-        
-        population = []
-        for I in args:
-            xi = I[0] + uniform.rvs(size=size)*(I[1]-I[0])
-            population.append(xi)
-        
-        return np.array(population).T
+    population = []
+    for I in args:
+        xi = I[0] + uniform.rvs(size=size)*(I[1]-I[0])
+        population.append(xi)
+
+    return np.array(population).T
 
 def cuckoo_search(f, nHosts, pa, ranges, maxIter=10**3, checkpointDir=None, loadCheckpoint=None, **kwargs):
     drop = int(round(nHosts*pa))
-    
+
+    currentState = checkpoint_state(checkpointDir=checkpointDir)
     if loadCheckpoint is not None:
-        print('Loading checkpoint {}'.format(loadCheckpoint))
-        pts = load_checkpoint(checkpointFile=loadCheckpoint)
+        print(f'Loading checkpoint {loadCheckpoint}')
+
+        currentState.load_state(checkpointFile=loadCheckpoint)
+
+        pts = currentState.data
         ptNum = len(pts)
         if ptNum > nHosts:
             pts = pts[:nHosts]
@@ -76,41 +141,47 @@ def cuckoo_search(f, nHosts, pa, ranges, maxIter=10**3, checkpointDir=None, load
         r = gen_population(nHosts, *ranges)
         print('Generating initial population...')
         pts = np.column_stack((r, f(r)))
-    
+
+    finalGen = maxIter + currentState.igen
     N = nHosts - drop
     topNOld = pts[:N].copy()
     for i in range(maxIter):
-        print('\n\n\nGENERATION {} OF {}'.format(i+1, maxIter))
+        currentState.update_gen_counter()
+        print(f'\n\n\nGENERATION {currentState.igen} OF {finalGen}')
         rNew = levy_advance(r0=pts[:,:-1], scale=1, **kwargs)
         rNew = periodic_bc(rNew, *ranges)
         print('\n\nLevy flight...')
         ptsNew = np.column_stack((rNew, f(rNew)))
-        
+
         np.random.shuffle(ptsNew)
-        
+
         pts = np.where(np.expand_dims(ptsNew[:,-1], 1) < np.expand_dims(pts[:,-1], 1), ptsNew, pts)
         pts = pts[pts[:,-1].argsort()]          
-        
+
         if (checkpointDir is not None) and ((pts[:N]!=topNOld).any() or i==maxIter-1):
             topNOld = pts[:N].copy()
-            save_checkpoint(checkpointArr=pts,
-                            outDir=checkpointDir, 
-                            gen=i+1)
-        
+            currentState.data = pts.copy()
+            currentState.save_state(fileType='text')
+
         rRand = gen_population(drop, *ranges)  
         print('\n\nRandom mutation...')
         ptsRand = np.column_stack((rRand, f(rRand)))
-        
-        pts[-drop:] = ptsRand
-        
-        best = pts[pts[:,-1].argmin()]
-        print('\n\nCURRENT BEST: chi2: {0} \nFracs: {1}\nRcut: {2}\nalpha: {3}\n'.format(best[-1],
-                                                                                         best[:-3]/best[:-3].sum(),
-                                                                                         rescale_from_normal(interval=rcutRange, 
-                                                                                                             value=best[-3]),
-                                                                                         rescale_from_normal(interval=alphaRange, 
-                                                                                                             value=best[-2])))
 
-    pts = pts[pts[:,-1].argsort()]
-    save_checkpoint(checkpointArr=pts, outDir=checkpointDir, gen='last')
+        pts[-drop:] = ptsRand
+
+        best = pts[pts[:,-1].argmin()]
+        print('\n\nCURRENT BEST: chi2: {0} \nFracs: {1}\nRcut: {2}\nalpha: {3}\n'.format(
+                                                                                        best[-1],
+                                                                                        best[:-3]/best[:-3].sum(),
+                                                                                        rescale_from_normal(interval=rcutRange, 
+                                                                                                            value=best[-3]),
+                                                                                        rescale_from_normal(interval=alphaRange, 
+                                                                                                            value=best[-2]))
+                                                                                        )
+        currentState.data = pts.copy()
+        currentState.save_state(fileType='pickle')
+
+    currentState.data = pts[pts[:,-1].argsort()]
+    currentState.save_state(fileType='text')
+
     return from_raw_to_real(arr=pts[:-drop])
--- scripts/src/optimization/objective_functions.py
+++ scripts/src/optimization/objective_functions.py
@@ -1,39 +1,54 @@
-import gc
-import numpy as np
-
-from tqdm import tqdm
+import shutil
+import pickle
+from uuid import uuid4
 from pathlib import Path
+
+import numpy as np
+from tqdm import tqdm
 
 from ..UHECRs_sim_f import A_Z
 from .. import auger_data_he as pao
 from ..utils.error_functions import err_parameter_handler
 from ..utils.general import (events_from_files, 
-                             rescale_from_normal,
-                             hidden_prints)
+                            rescale_from_normal,
+                            hidden_prints)
 
 rcutRange = [19., 21.]
-alphaRange = [1., 3.]
+alphaRange = [1., 5.]
 orderedNuclei = sorted(A_Z.keys(), key=lambda z: A_Z[z])
 
-def chi2_obj_func(runPropFunc, sample, pattern, bins=pao.ebins, **kwargs):
+def save_output(src, dst, toPickle):
+    shutil.copytree(src, dst, dirs_exist_ok=True)
+
+    with open(Path(dst).joinpath('params_objF_list.pkl'), 'wb') as handle:
+        pickle.dump(toPickle, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+def chi2_obj_func(runPropFunc, sample, pattern, chi2Thres, bins=pao.ebins, **kwargs):
     chi2Container = []
-    parts = kwargs.get('parts') if (kwargs.get('parts') is not None) else 1
-    for fracs, (rcut, alpha) in tqdm(zip(sample[:,:-2], sample[:,-2:])):
+
+    outDir = Path(kwargs.get('outDir'))
+
+    for fracs, (rcut, alpha) in zip(tqdm(sample[:,:-2]), sample[:,-2:]):
         nucleiDict = dict(zip(orderedNuclei, fracs))
-        print( 'Fracs: {}'.format(dict(zip(orderedNuclei, ['%.2e' % p for p in fracs/fracs.sum()]))) )
+
+        print(f"Fracs: {dict(zip(orderedNuclei, [f'{p:.1%}' for p in fracs/fracs.sum()]))}")
         with hidden_prints():
             runPropFunc(yamlFile=nucleiDict,
                         rcut=rescale_from_normal(interval=rcutRange, value=rcut),
                         alpha=rescale_from_normal(interval=alphaRange, value=alpha),
                         **kwargs)
-        
-        
-        eventFiles = Path(kwargs.get('outDir')).glob(pattern)
-        simN, lnAStats, lnAErrors = events_from_files(fileNames=eventFiles, bins=bins)
+
+        eventFiles = outDir.glob(pattern)
+        simN, _, __ = events_from_files(fileNames=eventFiles, bins=bins)
         idx = np.abs(pao.ebins-bins[0]).argmin()
-        print('Events per bin: {}'.format(simN))
+        print(f'Events per bin: {simN}')
         chi2 = err_parameter_handler(errorType='chi2', simN=simN, paoN=pao.auger[idx:idx+len(bins)])
         chi2Container.append(chi2)
-        print('Associated chi2: {:.2f}'.format(chi2))
-        gc.collect()
+        print(f'Associated chi2: {chi2:.2f}')
+
+        if chi2 <= chi2Thres:
+            save_output(src=outDir,
+                        dst=outDir.parent.joinpath(f'saved_optimal_solutions/{uuid4()}/'),
+                        toPickle=[*fracs, rcut, alpha, chi2])
+
     return np.array(chi2Container)
