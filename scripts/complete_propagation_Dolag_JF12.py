316391c7cf478b95dd01dfc43558d604904b72b0
CaratheodoryRR <cabanillas.christian@pucp.edu.pe>
2026-07-08 18:42:38 -0500

    fix: Removed unexistent TrajectoryLength in favor of CRPropa native MaximumTrajectoryLength

--- scripts/complete_propagation_Dolag_JF12.py
+++ scripts/complete_propagation_Dolag_JF12.py
@@ -148,7 +148,7 @@ def run_extra_galactic_part(filesDict, kwargsProp, srcType, partNum, kappa, **kw
     test_obs = Observer()
     test_obs.add(ObserverSurface( Sphere(galCenSGVec, prop_3D.farthestSourceDistance.get(srcType)) ))
     test_obs.add( MinimumEnergy( cpf.stopE ) )
-    test_obs.add( TrajectoryLength( cpf.maxTrajectoryL ) )
+    test_obs.add( MaximumTrajectoryLength( cpf.maxTrajectoryL ) )
     test_obs.setDeactivateOnDetection(True)
     sim.add(test_obs)
 
@@ -188,7 +188,7 @@ def run_galactic_part(filesDict, rObs, **kwargsSim):
     test_obs = Observer()
     test_obs.add(ObserverSurface( Sphere(Vector3d(0), 1.01*rGalaxy) ))
     test_obs.add( MinimumEnergy( cpf.stopE ) )
-    test_obs.add( TrajectoryLength( cpf.maxTrajectoryL ) )
+    test_obs.add( MaximumTrajectoryLength( cpf.maxTrajectoryL ) )
     test_obs.setDeactivateOnDetection(True)
     sim.add(test_obs)
 
